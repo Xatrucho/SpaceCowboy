297603fb049569d0a8175663849184a6903edfeb
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2022-01-19 11:24:16 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import coding from './cryptoBull.gif';
+
+const App = () => (
+  <div>
+    <h1 className="wow">Welcome!
+    </h1>
+    <img src={coding} alt="" />
+  </div>
+);
+  
+
+export default App;
