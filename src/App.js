3631bb48692b0d1bacf31f8d340572f5c63c6d51
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2022-01-28 13:34:32 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import coding from './cryptoBull.gif';
+// import React from 'react';
+// import './App.css';
+// import coding from './cryptoBull.gif';
 
-const App = () => (
-  <div>
-    <h1 className="wow">Welcome!
-    </h1>
-    <img src={coding} alt="" />
-  </div>
-);
+// const App = () => (
+//   <div>
+//     <h1 className="wow">Welcome!
+//     </h1>
+//     <img src={coding} alt="" />
+//   </div>
+// );
   
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import logo from "../src/images/spaceship-swordfish-freelance.jpeg"
+import code from "../src/images/space-matrix.jpeg"
+import './index.css';
+import './bg-styles.css';
+// import '../src/images/spaceship-swordfish-freelance.jpeg'
+// import "./App.css";
 
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+function Page() {
+  return (
+    <div className="bgimg">
+          <div className="spaceship-container">
+              <img src={logo} width="50%" height="50%" alt="logo" />
+          </div>
+
+          <div className="info-container">
+              <div className="shop-name">
+                <h1>Welcome to SwordFish Customs</h1>
+              </div>
+
+              <div className="my-title">
+                <img src={code} width="20%" height="50px" alt="logo" />
+                    <h2>Full Stack Web Development</h2>
+                    <h3>By: Marlon Ponce</h3>
+              </div>
+            </div>
+
+            <div className="language-container">
+                <div className="Back-End">
+                  <h3>Back-End:
+                      <ul>
+                        <li>Python3</li> 
+                        <li>MongoDB</li> 
+                        <li>Javascript</li>
+                        <li>MySQL</li> 
+                        <li>PHP</li> 
+                        <li>Node.js</li>
+                      </ul>
+                  </h3>
+                </div>
+
+                <div className="Front-End">
+                    <h3>Front-End:
+                      <ul>
+                        <li>SASS</li> 
+                        <li>React.js</li> 
+                        <li>HTML</li>
+                        <li>CSS</li> 
+                        <li>jQuery</li> 
+                        <li>Javascript</li> 
+                      </ul>
+                    </h3>
+                  </div>
+                </div>
+
+                <div className="footer">
+                    <h4>
+                      2022 Ponce Development
+                    </h4>
+                </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
 
